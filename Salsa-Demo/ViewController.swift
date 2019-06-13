7e75a9fb88b03ca4436fd92742b8cc13a633277e
Emil Safier <EMIL535@Gmail.com>
2019-06-13 16:41:14 -0700

    Initial Commit

---
+++ Salsa-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Salsa-Demo
+//
+//  Created by Emil Safier on 6/13/19.
+//  Copyright © 2019 Emil Safier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
